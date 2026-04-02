243a5951201f3053387c5d00b1d4c43ee450ebd8
blumpi677-ai <blumpi677@gmail.com>
2026-04-02 14:36:32 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
 pip install -r requirements.txt
+
 python manage.py collectstatic --noinput
 python manage.py migrate
+
+# 👇 create superuser automatically
+python manage.py shell << END
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'ankushkundu737@gmail.com', 'admin123')
+END
